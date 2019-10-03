f8cddb4723e7fff68538c91ae1f9f94f3edcb226
hajczek <i.hajczewska@gmail.com>
2019-10-03 17:43:04 +0200

    Build a structure of html code

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ function App() {
   return (
     <div className="App">
       <div id="quote-box">
-        <div id="text">
+        <q id="text">
           A random quote will display here.
-        </div>
+        </q>
+        <cite id="author">
+          The name of an author of this quote will display here.
+        </cite>
+        <button id="new-quote">New quote</button>
+        <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet quote</a>
       </div>
     </div>
   );
