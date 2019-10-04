e156e0316bfc70c29a44cb4283111ea5d557b572
hajczek <i.hajczewska@gmail.com>
2019-10-04 09:28:10 +0200

    Change functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div id="quote-box">
-        <q id="text">
-          A random quote will display here.
-        </q>
-        <cite id="author">
-          The name of an author of this quote will display here.
-        </cite>
-        <button id="new-quote">New quote</button>
-        <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet quote</a>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div id="quote-box">
+          <q id="text">
+            The random quote will display here.
+          </q>
+          <cite id="author">
+            The name of an author of this quote will display here.
+          </cite>
+          <button id="new-quote">New quote</button>
+          <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet quote</a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
