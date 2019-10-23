8659af243afcd7ca9d4b96bb6b8f8ad07e606e0c
hajczek <i.hajczewska@gmail.com>
2019-10-23 08:47:42 +0200

    Updates

---
+++ precache-manifest.f849dee2ccea937b0c53d1db18b1738e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c01a032a7c5d3a5c2f3cf05b2ea0cf",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "2637258a7756b284368d",
+    "url": "/random-quote-machine/static/css/main.f5459602.chunk.css"
+  },
+  {
+    "revision": "bce10ce95e93b37f7931",
+    "url": "/random-quote-machine/static/js/2.83410987.chunk.js"
+  },
+  {
+    "revision": "2637258a7756b284368d",
+    "url": "/random-quote-machine/static/js/main.feffd988.chunk.js"
+  },
+  {
+    "revision": "ba23cb9ee545fe67ab02",
+    "url": "/random-quote-machine/static/js/runtime-main.b170d7ea.js"
+  }
+]);
